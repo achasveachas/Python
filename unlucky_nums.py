55a015fac35f179c4dfe0ff92561dafae2d35ce1
Yechiel Kalmenson <contact@yechiel.me>
2018-09-02 18:54:07 -0400

    typo

--- unlucky_nums.py
+++ unlucky_nums.py
@@ -3,4 +3,4 @@ for num in range(1, 21):
         print(f"{num} in unlucky")
     else:
         oddness = "even" if num % 2 == 0 else "odd"
-        print (f"{num} is {oddness}")
+        print(f"{num} is {oddness}")
