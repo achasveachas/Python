aa838ab380d29f5b6bf78931f61afe72d406b35f
Yechiel Kalmenson <contact@yechiel.me>
2018-09-02 19:08:23 -0400

    refactor unlucky_nums

--- unlucky_nums.py
+++ unlucky_nums.py
@@ -1,6 +1,4 @@
 for num in range(1, 21):
-    if num == 4 or num == 13:
-        print(f"{num} in unlucky")
-    else:
-        oddness = "even" if num % 2 == 0 else "odd"
-        print(f"{num} is {oddness}")
+    state = "even" if num % 2 == 0 else "odd"
+    state = "unlucky" if num == 4 or num == 13 else state
+    print(f"{num} is {state}")
