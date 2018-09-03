01506e878cebfd8d5d8e27affb61908a36736c3e
Yechiel Kalmenson <contact@yechiel.me>
2018-09-03 17:07:26 -0400

    implement playlist as nested data structure

---
+++ playlist.py
@@ -0,0 +1,9 @@
+playlist = {
+    "name": "Patagonia",
+    "author": "Yechiel",
+    "songs": [
+        "title": "Song 1",
+        "Artist": ["Artist 1", "Artist 2"],
+        "Duration": 2.34
+    ]
+}
