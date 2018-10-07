128c35d629f97017459c80957ea777523a353526
Yechiel Kalmenson <contact@yechiel.me>
2018-10-07 10:57:15 -0400

    remove windows requirement

--- ascii_art.py
+++ ascii_art.py
@@ -1,8 +1,6 @@
 from termcolor import colored
 from pyfiglet import figlet_format
 from random import choice
-from colorama import init
-init()
 
 msg = input("Hello, what would you like to display? ")
 color = input("Which color would you like it in? ")
